6754593bb6d9dd90d23c7a1d070ab8a2560f0bdf
Brian-Weloba <bweloba@gmail.com>
2021-11-17 15:46:07 +0300

    Test database endpoints

--- src/main/java/com/flutterwave/Hamsoace/payment/PaymentController.java
+++ src/main/java/com/flutterwave/Hamsoace/payment/PaymentController.java
@@ -1,5 +1,6 @@
 package com.flutterwave.Hamsoace.payment;
 
+import com.flutterwave.Hamsoace.Repositories.PayloadRepository;
 import com.flutterwave.Hamsoace.models.Payload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -12,11 +13,13 @@ import java.util.Map;
 @RequestMapping("/api/v1")
 public class PaymentController {
     private final PaymentService paymentService;
+    private final PayloadRepository repository;
 
 
     @Autowired
-    public PaymentController(PaymentService paymentService){
+    public PaymentController(PaymentService paymentService, PayloadRepository repository) {
         this.paymentService = paymentService;
+        this.repository = repository;
     }
 
     @GetMapping("/pay")
@@ -40,5 +43,21 @@ public class PaymentController {
     @GetMapping("/test")
     public String test(){
         return "test";
+    }
+
+    @PostMapping("/database-pay")
+    public String databasePay(@ModelAttribute Payload payload, Model model, HttpServletRequest request) throws Exception{
+        if (request.getMethod().contentEquals("GET")){
+            return "redirect:";
+        }
+        paymentService.initialize(model);
+        repository.save(payload);
+        return "pay";
+    }
+
+    @GetMapping("/all")
+    public @ResponseBody Iterable<Payload> getAllPayloads() {
+        // This returns a JSON or XML with the users
+        return repository.findAll();
     }
 }
