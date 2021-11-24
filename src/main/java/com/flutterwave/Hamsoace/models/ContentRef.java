abdd9d35d131b7f1eda9e57cbaad070a40ccf1dc
Brian-Weloba <bweloba@gmail.com>
2021-11-24 10:42:38 +0300

    Add content Model

---
+++ src/main/java/com/flutterwave/Hamsoace/models/ContentRef.java
@@ -0,0 +1,13 @@
+package com.flutterwave.Hamsoace.models;
+
+public class ContentRef {
+    private String ref;
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+}
