b1cbdea09f784407e7db9bf1192aa30b8ecd6e0c
hamsoace <hamsoace@gmail.com>
2021-12-02 12:32:34 +0300

    Cancel page styling

--- src/main/java/com/flutterwave/Hamsoace/utils/URLs.java
+++ src/main/java/com/flutterwave/Hamsoace/utils/URLs.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 public class URLs {
 
     //1. Base URL
-    public static final String BASE_URL = "https://342e-102-222-146-74.ngrok.io";
+    public static final String BASE_URL = "https://b248-105-21-41-70.ngrok.io";
     //2. Microservice URL
-    public static final String MICROSERVICE_URL = "https://5367-102-222-146-74.ngrok.io";
+    public static final String MICROSERVICE_URL = "https://localhost:8081";
     //3. API Key
     @Value("${rave.api.key}")
     public static String API_KEY;
